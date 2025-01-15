1f261cb9512e39d7f999bc6e56baad984a7f145f
KOUNOIKE Yuusuke <yusuke.konoike@starley.jp>
2025-01-16 01:35:27 +0900

    dkms mkdebに頼らない.debのbuild

--- build_deb.sh
+++ build_deb.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+# requirements:
+# sudo apt-get install -y debhelper devscripts dh-exec dkms dpkg fakeroot
+
+set -xe
+
+version="0.5.4"
+cd $(dirname $0)
+
 set -CEuo pipefail
 
-# Prep
-sudo apt-get install -y debhelper devscripts dh-exec dkms dpkg
+WORKDIR=./work
+SRCDIR=${WORKDIR}/usr/src/px4_drv-${version}
+SHAREDIR=${WORKDIR}/usr/share/px4_drv-dkms
+
+[ -d ${WORKDIR} ] && rm -rf ${WORKDIR} && mkdir -p ${WORKDIR} && mkdir -p ${SRCDIR} && mkdir -p ${SHAREDIR}
+
+# prepare files
+cp -af ./dkms/DEBIAN ${WORKDIR}/
+cp -af ./driver ./etc ./fwtool ./include dkms.conf README.md LICENSE ${SRCDIR}
+cp -f ./dkms/postinst ${SHAREDIR}
 
 # Build dkms deb
-dkms mkdeb --source-only
+fakeroot dpkg-deb --build ${WORKDIR} .
--- update-versions.sh
+++ update-versions.sh
@@ -12,6 +12,13 @@ sed -i -e "s/^PACKAGE_VERSION=.*/PACKAGE_VERSION=\"$version\"/" dkms.conf
 # Update version in driver/driver_module.h
 sed -i -e "s/^#define\s\s*PX4_DRV_VERSION\s\s*.*/#define	PX4_DRV_VERSION	\"$version\"/" driver/driver_module.h
 
+# Update version in DEBIAN/control
+sed -i -e "s/^Version:.*/Version: $version/" dkms/DEBIAN/control
+sed -i -e "Provides: px4-drv-modules .*/Provides: px4-drv-modules (= $version)/" dkms/DEBIAN/control
+
+# Update version in build_deb.sh
+sed -i -e "s/^version=.*/version=\"$version\"/" build_deb.sh
+
 # Update version in winusb/src/BonDriver_PX4/resource.h
 sed -i -e "s/^#define\s\s*VER_FILE\s\s*.*/#define VER_FILE	${version//./,},0/" winusb/src/BonDriver_PX4/resource.h
 sed -i -e "s/^#define\s\s*VER_FILE_STR\s\s*.*/#define	VER_FILE_STR	\"$version\"/" winusb/src/BonDriver_PX4/resource.h
